fccb26d1e70ea2ed3ec52ed433672844c93f355e
Juan Carlos Fontecha <jcfontecha@gmail.com>
2016-03-13 18:21:15 -0600

    Initial Commit

---
+++ Deep Work Planner/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Deep Work Planner
+//
+//  Created by Juan Carlos Fontecha on 3/13/16.
+//  Copyright © 2016 Juan Carlos Fontecha. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
